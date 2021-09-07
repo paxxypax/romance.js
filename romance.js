a368a8de649190c4add3c66e2081ef693eb0372a
paxxypax <77129965+paxxypax@users.noreply.github.com>
2021-09-06 21:30:57 -0400

    Update romance.js

--- romance.js
+++ romance.js
@@ -1,27 +1,34 @@
 //first need to create a word corpus. i will do this using the project's example of the lyrics from Hotline Bling. 
 
 //first function to break the words down from a pre-existing string into individual strings to use to write the poetry
+//EDIT: 9.6.21 current test is to the lyrics from blonde redhead's "the dress"
 function parseText(text){
-let words = text.toLowerCase().split(' ');
-return words;
+    let words = text.toLowerCase().split(' ');
+    return words;
 }
 
-let words = parseText("Ever since I left the city you you you You and me we just don't get along");
+let words = parseText("Tears you see on my face you do have something to do with Fear starts creeping up when you have so much to lose");
 
 
 //create key/values of word pairs to string together words next to each other that sound natural together
+//EDIT: 9.6.21 need to build an array of two words per key for more dynamic lines of poetry
 function generateWordPairs(words){
-let wordPairs = {};
-for (let j = 0; j<words.length-1; j++){
-    wordPairs[words[j]]=[];
-}
-for (let i = 0; i<words.length-1; i++){
-    wordPairs[words[i]].push(words[i+1]); 
+    let wordPairs = {};
+    for (let j = 0; j<words.length-1; j++){
+        wordPairs[words[j]]=[];
     }
-    return wordPairs;
+    for (let i = 0; i<words.length-1; i++){
+        wordPairs[words[i]].push(words[i+1]);
+        if(words[i+2]!== undefined){
+            wordPairs[words[i]].push(words[i+2]);
+        }
+    }
+return wordPairs;
 }
+
 let wordPairs = generateWordPairs(words);
-console.log(wordPairs);
+
+let punctuation = [',', ';', '!', '?'] //punctuation reference
 
 //next function will need a helper function
 function getRandomInt(max) {
@@ -33,28 +40,32 @@ function writeLine(wordPairs,num){
     let objectKeys = Object.keys(wordPairs); 
     let currentWord = objectKeys[getRandomInt(objectKeys.length)];
     for(let i = 0; i<num ; i++){
-        line += currentWord + ' ';
+        line += currentWord;
         let currentList = wordPairs[currentWord];
         if(wordPairs[currentWord] === undefined){
-            currentWord = "just";
+            currentWord = objectKeys[getRandomInt(objectKeys.length)];
         }
         else {
             let testNum = currentList.length;
             let randomNumber = getRandomInt(testNum);
             currentWord = currentList[randomNumber];
         }
+        if(Math.random()>.5){ //add random punctuation
+            line += punctuation[getRandomInt(punctuation.length)]
+        }
+        line += ' ';
     }
     return line;
 }
-let line = writeLine(wordPairs,8); 
 
 //write the whole poem
-function generatePoem(line, numLine){
+function generatePoem(numLine){
     let poem = ''; 
     for (let i = 0; i < numLine; i++){
-            poem += line + "\n";
+        let line = writeLine(wordPairs, getRandomInt(15)); //call randomizer function for random amounts of words in code
+        poem += line + '\n';
     }
     return poem;
 }
 
-generatePoem(line, 6);
+generatePoem(6);
